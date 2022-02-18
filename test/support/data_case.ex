43c1bab6c09bc717544aa41a0ab9d94c7b056604
Lucas Stellet <lustepe@gmail.com>
2022-02-18 10:59:54 -0300

    chore add new configs

--- test/support/data_case.ex
+++ test/support/data_case.ex
@@ -24,6 +24,7 @@ defmodule Genpagx.DataCase do
       import Ecto.Changeset
       import Ecto.Query
       import Genpagx.DataCase
+      import Genpagx.Factory
     end
   end
 
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,2 +1,9 @@
 ExUnit.start()
+
+# Faker start
+Faker.start()
+
+# ex_machina start
+{:ok, _} = Application.ensure_all_started(:ex_machina)
+
 Ecto.Adapters.SQL.Sandbox.mode(Genpagx.Repo, :manual)
